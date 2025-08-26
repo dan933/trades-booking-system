846d6f08a5e108ca9507c77dbc9183a631035f64
dan933 <dnadistrictservices@gmail.com>
2025-08-26 20:07:10 +1000

    styling changes for login page

--- admin-site/src/app/shared/auth/auth.component.ts
+++ admin-site/src/app/shared/auth/auth.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnDestroy, OnInit, inject } from '@angular/core';
-import { Auth, idToken, signInWithEmailAndPassword } from '@angular/fire/auth';
+import {
+  Auth,
+  idToken,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
@@ -7,7 +12,7 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit, OnDestroy {
   private auth: Auth = inject(Auth);
@@ -17,19 +22,18 @@ export class AuthComponent implements OnInit, OnDestroy {
   loginForm: FormGroup = new FormGroup({});
   loading = false;
 
+  view = 'login';
 
-  constructor(
-    public router: Router,
-    private fb: FormBuilder
-  ) {
-    this.idTokenSubscription = this.idToken$.subscribe((token: string | null) => {
-      //handle idToken changes here. Note, that user will be null if there is no currently logged in user.
-      if (token) {
-      //If user is logged in, navigate to home page
-      this.router.navigate(['/schedule']);
+  constructor(public router: Router, private fb: FormBuilder) {
+    this.idTokenSubscription = this.idToken$.subscribe(
+      (token: string | null) => {
+        //handle idToken changes here. Note, that user will be null if there is no currently logged in user.
+        if (token) {
+          //If user is logged in, navigate to home page
+          this.router.navigate(['/schedule']);
+        }
       }
-    })
-
+    );
   }
   ngOnDestroy(): void {
     this.idTokenSubscription.unsubscribe();
@@ -37,12 +41,33 @@ export class AuthComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      password: ['', Validators.required],
     });
   }
 
   forgotPassword() {
+    this.view = 'reset';
+  }
 
+  async resetPassword(event: Event) {
+    event.preventDefault();
+    
+    if (this.loginForm.get('email')?.valid) {
+      this.loading = true;
+      try {
+        await sendPasswordResetEmail(this.auth, this.loginForm.value['email']);
+        this.loginForm.setErrors(null);
+        this.view = 'login';
+        this.loading = false;
+      } catch (error: any) {
+        this.loginForm.setErrors({ failed: true });
+        this.loading = false;
+      }
+    }
+  }
+
+  backToLogin() {
+    this.view = 'login';
   }
 
   login(event: Event) {
@@ -50,32 +75,39 @@ export class AuthComponent implements OnInit, OnDestroy {
 
     if (this.loginForm.valid) {
       this.loading = true;
-      signInWithEmailAndPassword(this.auth, this.loginForm.value['email'], this.loginForm.value['password']).then((userCredential: any) => {
+      signInWithEmailAndPassword(
+        this.auth,
+        this.loginForm.value['email'],
+        this.loginForm.value['password']
+      )
+        .then((userCredential: any) => {
+          console.log(
+            this.loginForm.value['email'],
+            this.loginForm.value['password']
+          );
+          //reset errors
+          this.loginForm.setErrors(null);
+          this.loading = false;
 
-        console.log(this.loginForm.value['email'], this.loginForm.value['password'])
-        //reset errors
-        this.loginForm.setErrors(null);
-        this.loading = false;
-
-        //Navigate to home page
-        this.router.navigate(['/schedule']);
-
-      }).catch((error) => {
-        console.log(error?.message, error?.code);
-        if (error?.code === 'auth/user-not-found' || error?.code === 'auth/wrong-password') {
-          this.loginForm.setErrors({ 'incorrect': true });
-        } else {
-          this.loginForm.setErrors({ 'failedAttempts': true });
-        }
-        this.loading = false;
-      });
+          //Navigate to home page
+          this.router.navigate(['/schedule']);
+        })
+        .catch((error) => {
+          console.log(error?.message, error?.code);
+          if (
+            error?.code === 'auth/user-not-found' ||
+            error?.code === 'auth/wrong-password'
+          ) {
+            this.loginForm.setErrors({ incorrect: true });
+          } else {
+            this.loginForm.setErrors({ failedAttempts: true });
+          }
+          this.loading = false;
+        });
     }
-
-
   }
   toggleHide(event: Event) {
     event.preventDefault();
     this.hide = !this.hide;
   }
-
 }
