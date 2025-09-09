a745fe7d434b5bb60d40cb347215edbe416b3053
dan933 <dnadistrictservices@gmail.com>
2025-09-09 20:38:32 +1000

    apex charts added dashboard front end layout added

--- admin-site/src/app/components/dashboard/dashboard.component.ts
+++ admin-site/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
 // import { Auth } from '@angular/fire/auth';
 
 @Component({
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit, AfterViewInit {
   // constructor(private auth: Auth) {
   //   const currentUser = this.auth.currentUser;
   //   console.log(currentUser);
@@ -17,4 +17,108 @@ export class DashboardComponent {
   //completed jobs
   //chart of revenue
   //todays schedule
+
+  screenWidth: number = 0;
+  chart: any;
+
+  ngOnInit() {
+    this.screenWidth = window.innerWidth; // Set initial width
+  }
+
+  renderChart() {
+    if (this.chart) {
+      this.chart.destroy();
+    }
+
+    const targetElement = this.screenWidth < 1261 ? '#chart-mobile' : '#chart';
+
+    const options = {
+      theme: {
+        mode: 'dark',
+      },
+      series: [
+        {
+          name: 'Revenue',
+          data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+        },
+      ],
+      chart: {
+        type: 'line',
+        height: this.screenWidth < 1261 ? 300 : 500,
+        background: 'transparent',
+        toolbar: {
+          show: false,
+        },
+        zoom: {
+          enabled: false,
+          type: 'x',
+          autoScaleYaxis: true,
+          zoomedArea: {
+            fill: {
+              color: '#90CAF9',
+              opacity: 0.4,
+            },
+            stroke: {
+              color: '#0D47A1',
+              opacity: 0.4,
+              width: 1,
+            },
+          },
+        },
+        pan: {
+          enabled: true,
+          type: 'x',
+        },
+        selection: {
+          enabled: true,
+          type: 'x',
+        },
+        events: {
+          beforeMount: function (chartContext: any, config: any) {
+            // Disable passive event listeners warning
+          },
+        },
+      },
+      xaxis: {
+        categories: [
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+        ],
+      },
+    };
+
+    this.chart = new (window as any).ApexCharts(
+      document.querySelector(targetElement),
+      options
+    );
+
+    this.chart.render();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    console.log('window.innerWidth', window.innerWidth);
+
+    const currentWidth = +this.screenWidth;
+
+    this.screenWidth = window.innerWidth;
+
+    if (currentWidth < 1261 && window.innerWidth >= 1261) {
+      console.log('render');
+      this.renderChart();
+    } else if (currentWidth >= 1261 && window.innerWidth < 1261) {
+      this.renderChart();
+    }
+  }
+
+  ngAfterViewInit() {
+    this.renderChart();
+  }
 }
